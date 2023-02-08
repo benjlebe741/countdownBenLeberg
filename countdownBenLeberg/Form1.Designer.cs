63d1f28e701531dc90ecd18fb6e2a418259b78bd
benjlebe741 <benjlebe741@ed.amdsb.ca>
2023-02-07 22:31:26 -0500

    Add project files.

---
+++ countdownBenLeberg/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace countdownBenLeberg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playButton = new System.Windows.Forms.Button();
+            this.countdownLabel = new System.Windows.Forms.Label();
+            this.stateControlTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.Font = new System.Drawing.Font("MS Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.Location = new System.Drawing.Point(276, 62);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(258, 119);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "P L A Y";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            this.playButton.MouseLeave += new System.EventHandler(this.playButton_MouseLeave);
+            this.playButton.MouseHover += new System.EventHandler(this.playButton_MouseHover);
+            // 
+            // countdownLabel
+            // 
+            this.countdownLabel.Font = new System.Drawing.Font("Castellar", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.countdownLabel.Location = new System.Drawing.Point(230, 221);
+            this.countdownLabel.Name = "countdownLabel";
+            this.countdownLabel.Size = new System.Drawing.Size(348, 142);
+            this.countdownLabel.TabIndex = 1;
+            this.countdownLabel.Text = ". . .";
+            this.countdownLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // stateControlTimer
+            // 
+            this.stateControlTimer.Enabled = true;
+            this.stateControlTimer.Interval = 200;
+            this.stateControlTimer.Tick += new System.EventHandler(this.stateControlTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.countdownLabel);
+            this.Controls.Add(this.playButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label countdownLabel;
+        private System.Windows.Forms.Timer stateControlTimer;
+    }
+}
+
---
+++ countdownBenLeberg/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+namespace countdownBenLeberg
+{
+    public partial class Form1 : Form
+    {       
+        private int buttonState = 1;
+        SoundPlayer player = new SoundPlayer(Properties.Resources.waveAudio);
+        SoundPlayer beepboop = new SoundPlayer(Properties.Resources.beepboopAudio);
+        SoundPlayer cymbol = new SoundPlayer(Properties.Resources.cymbolAudio);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void buttonGreen() {
+            playButton.Text = "P  L  A  Y\nP  L  A  Y\nP  L  A  Y";
+            playButton.ForeColor = Color.White;
+            playButton.BackColor = Color.Green;
+            beepboop.Play();
+        }
+
+        private void wait(int time) {
+            Refresh();
+            Thread.Sleep(time);
+        }
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            buttonState = 3;
+        }
+
+        private void playButton_MouseHover(object sender, EventArgs e)
+        {
+            buttonState = 2;
+            buttonGreen();
+        }
+
+        private void playButton_MouseLeave(object sender, EventArgs e)
+        {
+            buttonState = 1;
+        }
+        //Hi Mr T! I was talking to some friends who took this course last semester about trying to make a button
+        //which switches from black and green constantly, and they advised me to look at timers in the oneNote!
+        //I know this is a little jenky but it allowed me to have something almost like a: while (1==1) loop :) 
+        private void stateControlTimer_Tick(object sender, EventArgs e)
+        {
+            //if the mouse is not on the button
+            if (buttonState == 1)
+            {
+                playButton.Text = "P L A Y";
+                playButton.ForeColor = Color.Black;
+                playButton.BackColor = Color.White;
+            }
+            //if the mouse is on the button
+            if (buttonState == 2)
+            {
+                buttonGreen();
+                wait(stateControlTimer.Interval / 2);
+                playButton.BackColor = Color.Black;
+            }
+            //after the button is pressed
+            if (buttonState == 3)
+            {
+                playButton.Enabled = false;
+                playButton.Visible = false;
+                countdownLabel.BackColor = Color.Black;
+                BackColor = Color.Black;
+                wait(1700);
+                countdownLabel.ForeColor = Color.White;
+                wait(500);
+                countdownLabel.Text = ". .";
+                wait(500);
+                countdownLabel.Text = ".";
+                wait(500);
+                countdownLabel.Text = "";
+                wait(3000);
+                for (int x = 3; x > 0; x--) {
+                    player.PlaySync();
+                    countdownLabel.Text = x.ToString();
+                    wait(1250);
+                }
+                wait(250);
+                cymbol.Play();
+                countdownLabel.Text = "G O";
+                for (int x = 12; x < 75; x++)
+                {
+                    countdownLabel.ForeColor= Color.White;
+                    //I also looked up how to set a font size! I just wanted you to know im not copying
+                    //anyone-- I was just looking into a few extra things! thank you!
+                    countdownLabel.Font = new Font("Castellar", x, FontStyle.Bold);
+                    wait(5);
+                    countdownLabel.ForeColor = Color.Black;
+                    wait(5);
+                }
+                countdownLabel.ForeColor = Color.Black;
+            }
+
+        }
+    }
+}
